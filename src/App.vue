f5d398bc8ac3e7cfe83f46b613e8886e66259cf4
yuhang <zyh200286@qq.com>
2022-03-22 12:46:59 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components:{ Header}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='container'>
+     <h1>我是Header</h1>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name:'Header',
+    setup(){
+
+    }
+  }
+</script>
+  
+<style scoped lang='scss'>
+  .container{
+    background-color: coral;
+  }
+</style>
